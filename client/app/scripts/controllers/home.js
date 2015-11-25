cba53e46e6d0176752f89f9f2cfc384d3e4a2327
Theo <theo.a.sherman@gmail.com>
2015-11-24 22:35:55 -0600

    wired up client dashboard with server routes.

--- client/app/scripts/controllers/home.js
+++ client/app/scripts/controllers/home.js
@@ -1,9 +1,23 @@
 'use strict';
 
 angular.module('app').controller('HomeCtrl', function ($scope, $http, ngToast) {
-  $scope.stats = {};
+  $scope.awaitingClientSignatures = {};
+  $scope.awaitingOwnerSignatures = {};
+  $scope.completedSignatures = {};
   
-  $http.get('api/signatures/stats').success(function (stats) {
-    $scope.stats = stats;
+  $http.get('api/signatures/awaitingClient').success(function (signatures) {
+    $scope.awaitingClientSignatures = signatures;
   });
+  
+  $http.get('api/signatures/awaitingOwner').success(function (signatures) {
+    $scope.awaitingOwnerSignatures = signatures;
+  });
+  
+  $http.get('api/signatures/completed').success(function (signatures) {
+    $scope.completedSignatures = signatures;
+  });
+  
+  $scope.fromNow = function (date) {
+		return moment(date).fromNow();
+	};
 });
